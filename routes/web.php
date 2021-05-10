93c60bdc7d9e49efc08e4f125924c3364ab90d37
DraganM2003 <mijatovic.dragan100@gmail.com>
2021-05-10 17:19:56 +0200

    com

--- routes/web.php
+++ routes/web.php
@@ -33,6 +33,7 @@ Route::get('/editPovez/{id}', [PovezController::class,'edit'])->name("povez.edit
 
 Route::post('/updatePovez/{id}', [PovezController::class,'update'])->name("povez.update");
 
+//kategorije
 Route::get('/settingsKategorije', [KategorijeController::class,'index'])->name("kategorije");
 
 Route::get('/createKategorije', [KategorijeController::class,'create'])->name("kategorije.create");
@@ -44,3 +45,16 @@ Route::get('/deleteKategorije/{id}', [KategorijeController::class,'delete'] )->n
 Route::get('/editKategorije/{id}', [KategorijeController::class,'edit'])->name("kategorije.edit");
 
 Route::post('/updateKategorije/{id}', [KategorijeController::class,'update'])->name("kategorije.update");
+
+//format
+Route::get('/format',[FormatController::class,'index'])->name("format");
+
+Route::get('/format-create',[FormatController::class,'create'])->name("format.create");
+
+Route::post('/format-save',[FormatController::class,'store'])->name("format.save");
+
+Route::get('/format-edit/{id}',[FormatController::class,'edit'])->name("format.edit");
+
+Route::post('/format-update/{id}', [FormatController::class,'update'])->name("format.update");
+
+Route::get('/format-delete/{id}',[FormatController::class,'delete'])->name("format.delete");
